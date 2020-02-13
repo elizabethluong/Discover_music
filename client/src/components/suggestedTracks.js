f3ab786820bbff908e13d1a8de84def8b20c4f83
Elizabeth luong <elizabethluong@live.co.uk>
2020-02-13 11:50:08 +0000

    add styling

--- client/src/components/suggestedTracks.js
+++ client/src/components/suggestedTracks.js
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export default function suggestedTracks(props) {
-let suggestedTracks = props.state.returnTracks
+  let suggestedTracks = props.state.returnTracks;
 
-    return (
-
- 
-      <div className="artists">
-        {suggestedTracks.length > 1
-          ? suggestedTracks.map(item => (
-              <div
-                className="container backImage"
-                style={{
-                  backgroundImage: `url(${item.album.images[0].url})`
-                }}
-                key={item.id}
-              ></div>
-            ))
-          : null}
-      </div>
-
-
-    )
+  return (
+    <div className="artists">
+      {suggestedTracks.length > 1
+        ? suggestedTracks.map(item => (
+            <div class="tile is-ancestor">
+              <div class="tile is-parent">
+                <article class="tile is-child box">
+                  <div
+                    className="container backImage"
+                    style={{
+                      backgroundImage: `url(${item.album.images[0].url})`
+                    }}
+                    key={item.id}
+                  ></div>
+                </article>
+              </div>
+            </div>
+          ))
+        : null}
+    </div>
+  );
 }
