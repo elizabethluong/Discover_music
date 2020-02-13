e2bb1a29f22efba02216168a88888cf43b6800b8
Elizabeth luong <elizabethluong@live.co.uk>
2020-02-13 15:16:50 +0000

    add spotify open in browser functionality

--- client/src/components/suggestedTracks.js
+++ client/src/components/suggestedTracks.js
@@ -7,11 +7,16 @@ export default function suggestedTracks(props) {
     <div className="artists">
       {suggestedTracks.length > 1
         ? suggestedTracks.map(item => (
-            <a key={item.id} href={item.album.uri}>
+            <a
+              key={item.id}
+              target="_blank"
+              href={`https://open.spotify.com/album/${item.album.id}?${item.album.uri}`}
+            >
               <div key={item.id} className="tile is-ancestor">
                 <div key={item.id} className="tile is-parent">
                   <article key={item.id} className="tile is-child box">
-                    <div key={item.id}
+                    <div
+                      key={item.id}
                       className="container backImage"
                       style={{
                         backgroundImage: `url(${item.album.images[0].url})`
